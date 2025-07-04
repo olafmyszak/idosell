010b8800a08b726c50f97f31315d9049141644ed
Olaf Myszak <myszakolaf@gmail.com>
2025-07-04 16:20:59 +0200

    Add basic auth

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 import express from "express";
 import { fetchOrders } from "./service/order.service.js"
 import ordersRouter from "./route/order.route.js"
+import expressBasicAuth from "express-basic-auth";
 
 const app = express();
 const port = 3000;
 
 fetchOrders().then(
-  console.log("Fetched orders data")
+  console.log(`Fetched orders data at ${new Date().toLocaleString()}`)
 )
 
 // Fetch data daily
-setInterval(fetchOrders, 24 * 60 * 60 * 1000);
+setInterval(() => {
+  fetchOrders().then(
+    console.log(`Fetched orders data at ${new Date().toLocaleString()}`)
+  )
+}, 24 * 60 * 60 * 1000);
 
 app.use(express.json())
-
+app.use(expressBasicAuth({
+  users: { 'admin': 'admin' }
+}))
 app.use("/orders", ordersRouter);
 
 app.listen(port, () => {
