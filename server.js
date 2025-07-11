e65c635e6b22f6ee75fa74bd013ac312acdc16dd
olafmyszak <myszakolaf@gmail.com>
2025-07-11 20:44:37 +0200

    Wait for fetchOrders() to finish before starting the server and cron job

--- server.js
+++ server.js
@@ -2,24 +2,29 @@ import app from "./app.js";
 import { fetchOrders } from "./service/orders.service.js";
 
 const port = 3000;
-
 const oneDay = 24 * 60 * 60 * 1000;
+const oneHour = 60 * 60 * 1000;
+
 try {
   await fetchOrders(oneDay);
 } catch (error) {
   console.error("Failed to fetch orders data:", error.message);
 }
 
-// Fetch data daily, checking every hour if a day has passed
-const oneHour = 60 * 60 * 1000;
-setInterval(async () => {
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+scheduleFetchOrders();
+
+async function scheduleFetchOrders() {
   try {
     await fetchOrders(oneDay);
   } catch (error) {
     console.error("Failed to fetch orders data:", error.message);
+  } finally {
+    setTimeout(() => {
+      scheduleFetchOrders();
+    }, oneHour);
   }
-}, oneHour);
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+}
